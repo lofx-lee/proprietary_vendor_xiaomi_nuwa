91dde6f41b99c00e1e99b978224559be220c87bf
SkyMinus <minus_rav@qq.com>
2025-02-03 11:23:43 +0800

    nuwa: Initial vendor tree for common
    Signed-off-by: SkyMinus <minus_rav@qq.com>

---
+++ Android.mk
@@ -0,0 +1,30 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),nuwa)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,fb4aae9b2f245e3d26b4349187a4dc106eef5fa8)
+$(call add-radio-file-sha1-checked,radio/aop.img,fc0c6800ddae7eeccd0d0d18ceab21d5c28d7cd5)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,8863f15ac5e9bc503af75c64e7185824cd03c019)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,8b88afd52059e0b36ce40566d67fd81ebf3d8c5f)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,4760c21df8727cef7d894850adb5b45300ecd140)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,ad848d70d6afd0d4073af479fc646aecfcb148d5)
+$(call add-radio-file-sha1-checked,radio/dsp.img,45dd089e194b9915a2fca6ac9032748d75cf2bf2)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,e83880cf20b468eb05066462df956ff013894d50)
+$(call add-radio-file-sha1-checked,radio/hyp.img,f1b2ff94d38a640d2264ce49a8d8252f7ef1af33)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,4068534e205053d8f32477020e83286bfc085912)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,2d8d7cb78ab20781c6155aaf7ef57a18c8ed8f6a)
+$(call add-radio-file-sha1-checked,radio/modem.img,1b2c28f02694b0fa0ca0a38148405aae9b87feea)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,652f3f1693a3620955e783c7e55a5a6fca5bee0b)
+$(call add-radio-file-sha1-checked,radio/shrm.img,ebdf3d601f4ba1fe149b570bffe33c0689c31549)
+$(call add-radio-file-sha1-checked,radio/tz.img,776083323e32ec3a3ce0330a115c3f72c4e98135)
+$(call add-radio-file-sha1-checked,radio/uefi.img,166952057d844d463c1f5e6e1fd42965cf79f3fb)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,6b38eb232ffc21d1e8aa7c2492e8237275f4cfc2)
+$(call add-radio-file-sha1-checked,radio/xbl.img,2368b997fd3e90b3fdd5c50c5044e92c4d59d746)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,4b2cb6291505894db98ab103c5b6d9432967e6b3)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,12b1f90431e21ad2e74bb56e269a6eeab44dfaef)
+
+endif
