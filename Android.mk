6ec315d73913e9b67e027bdfee05a1cfa14803cf
SkyMinus <minus_rav@qq.com>
2025-07-12 11:32:38 +0800

    nuwa: Initial vendor blobs from OS2.0.203.0.VMBCNXM
    Change-Id: If711554fd0c4b534aad2ec3d6de5cde2bcccd0d5

---
+++ Android.mk
@@ -0,0 +1,30 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),nuwa)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,55dd7e33b41f23bb89deb62eec3b6e65390fde27)
+$(call add-radio-file-sha1-checked,radio/aop.img,b9bff0822ce28bac0905f299bcac83168cbaea9e)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,0891b1d43a90ea4007e1fbca8d035f4ddb5837f4)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,1c6bdc61ed273690a3e59d53e08fb88f551a9d93)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,2384a65fb99e462c4fbf2be5fa28bc7f9d3b7549)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,0fe6583838b6bbd72bbde6f0b529cd1f13431b5d)
+$(call add-radio-file-sha1-checked,radio/dsp.img,7a895e75688ad33b00f1710eb450e3679b3040a1)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,99b64c4d3a2fd92d8aa34ccd48b4c115fbd3cee5)
+$(call add-radio-file-sha1-checked,radio/hyp.img,8dc26240c639e55f9fb2ba7156af41f6fe05c7f1)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,0d833d0c259b3d993e1cec802908d71bb15e6a69)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,f64173fc94c251eef2d013283e4c8c7f3332b493)
+$(call add-radio-file-sha1-checked,radio/modem.img,985f6a42799cc15986cda7da1478f301e33c442a)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,2b452721243e8a3bd63524d80080aa829be5e640)
+$(call add-radio-file-sha1-checked,radio/shrm.img,3a3c4687fcb91c3474b75272257fd25cf6b188e8)
+$(call add-radio-file-sha1-checked,radio/tz.img,6952420e564ef04ebbcf43d3a15e5afb974bd376)
+$(call add-radio-file-sha1-checked,radio/uefi.img,6a0035dcc80207b54aed42771ba9c7958e2f4322)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,57fe1dd558f1f05f5eef86ed0e96ce3e091f7ecb)
+$(call add-radio-file-sha1-checked,radio/xbl.img,f3a12db2efeaca633405f5f46ed10f51bac6f917)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,b1cb33361be4b83df0949be517503e4505b83a3a)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,bf8bcc508a29bf25d12853f083a8ae6a102ec30c)
+
+endif
